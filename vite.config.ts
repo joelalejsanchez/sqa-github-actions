4a4ad865f7db3e378b0f7955b2163adc2d8abdc9
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2025-11-16 21:11:49 -0400

    cambio de ruta base, y cliente smtp

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/sqa-github-actions/",
   test: {
     environment: "jsdom",
     globals: true,
